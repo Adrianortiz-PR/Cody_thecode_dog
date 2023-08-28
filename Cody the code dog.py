db3362fc76333098183506e4bacb6cedc7258207
Adrian Ortiz <113073917+Adrianortiz-PR@users.noreply.github.com>
2023-08-28 00:28:21 -0400

    Add files via upload

--- Cody the code dog.py
+++ Cody the code dog.py
@@ -1,36 +1,40 @@
-def dog():
- print("  /^-----^\       ")
- print("  V  o o  V       ")
- print("   |  Y  |        ")
- print("    \ Q /         ")
- print("    / - \         ")
- print("    |    \        ")
- print("    |     \     ) ")
- print("    || (___\====  ")
+def print_dog():
+    print("  /^-----^\       ")
+    print("  V  o o  V       ")
+    print("   |  Y  |        ")
+    print("    \ Q /         ")
+    print("    / - \         ")
+    print("    |    \        ")
+    print("    |     \     ) ")
+    print("    || (___\====  ")
 
+def play_game():
+    F1_moves = ["rock", "paper", "scissors"]
+    F2_moves = ["rock", "paper", "scissors"]
 
-    
-User=input("Hello, I'm Cody the code dog!! What is your name?: ")
-dog()
-print("Hello!", User, "would you like to play rock,paper and scissors? ")
+    user_input = input("Hello, I'm Cody the code dog!! What is your name?: ")
+    print_dog()
+    print("Hello,", user_input, "would you like to play rock, paper, and scissors? ")
+
+    while True:
+        F1 = input("I made my move. What will you pick? ")
+        F2 = random.choice(F2_moves)
+
+        if F1 == F2:
+            print(F1, "and", F2, "It's a draw!")
+        elif (F1 == "rock" and F2 == "scissors") or \
+             (F1 == "scissors" and F2 == "paper") or \
+             (F1 == "paper" and F2 == "rock"):
+            print("Wow! Well played, your", F1, "beats my", F2 + "!")
+        else:
+            print("Sorry! My", F2, "beats your", F1 + "!")
+
+        play_again = input("Do you want to play again? (yes/no): ")
+        if play_again.lower() != "yes":
+            print("Thanks for playing! Goodbye!")
+            break
+
 import random
-while True:
- F1moves=["rock","paper","scissors"]
- F2moves=["rock","paper","scissors"]
- F1=input("I made my move what will you pick? ")
- F2=random.choice(F2moves)
 
- if F1==F2:
-    print(F1, "and", F2, "looks like a draw!")
- elif F1=="rock" and F2 == "scissors":
-    print("Wow! well played your rock beats my scissors!")
- elif F1=="scissors" and F2 == "paper":
-    print("Wow! well played your scissors beats my paper!")
- elif F1=="paper" and F2 == "rock":
-    print("Wow! well played your paper beats my rock")
- elif F1=="rock" and F2 == "paper":
-    print("Sorry! paper beats rock!")
- elif F1=="paper" and F2 == "scissors":
-    print("Sorry! scissors beats paper!")
- elif F1=="scissors" and F2 =="rock":
-    print("Sorry! rock beats scissors!")
+if __name__ == "__main__":
+    play_game()
